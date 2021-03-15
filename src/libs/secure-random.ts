6d0ba3ec5a4f6b2480b5cf504da608d89efb283e
Aleksandr Kuvakin <sa-ny_95@mail.ru>
2021-03-15 14:30:17 +0300

    react native support

--- src/libs/secure-random.ts
+++ src/libs/secure-random.ts
@@ -1,63 +1,22 @@
-declare let exports: any;
-declare let module: any;
+import * as randomBytes from '@consento/sync-randombytes'
 
 declare const Buffer: any;
 
-
-function nodeRandom(count, options) {
-    const crypto = eval('require')('crypto');
-    const buf = crypto.randomBytes(count);
+function secureRandom(count, options) {
+    const randomUint8Array = (randomBytes as any)(new Uint8Array(count))
 
     switch (options.type) {
-        case 'Array':
-            return [].slice.call(buf);
-        case 'Buffer':
-            return buf;
-        case 'Uint8Array':
-            const arr = new Uint8Array(count);
-            for (let i = 0; i < count; ++i) {
-                arr[i] = buf.readUInt8(i);
-            }
-            return arr;
-        default:
-            throw new Error(options.type + ' is unsupported.');
-    }
-}
-
-function browserRandom(count, options) {
-    const nativeArr = new Uint8Array(count);
-    const crypto = self.crypto || (self as any).msCrypto;
-    crypto.getRandomValues(nativeArr);
-
-    switch (options.type) {
-        case 'Array':
-            return [].slice.call(nativeArr);
         case 'Buffer':
             try {
                 const b = new Buffer(1);
             } catch (e) {
                 throw new Error('Buffer not supported in this environment. Use Node.js or Browserify for browser support.');
             }
-            return new Buffer(nativeArr);
+            return new Buffer(randomUint8Array);
         case 'Uint8Array':
-            return nativeArr;
+            return randomUint8Array;
         default:
-            throw new Error(options.type + ' is unsupported.');
-    }
-}
-
-function secureRandom(count, options) {
-
-    options = options || {type: 'Array'};
-
-    if (typeof window === 'object' && typeof window.document === 'object') {
-        return browserRandom(count, options);
-    }
-    else if (typeof exports === 'object' && typeof module !== 'undefined') {
-        return nodeRandom(count, options);
-    }
-    else {
-        throw new Error('Your environment is not defined');
+            return [].slice.call(randomUint8Array);
     }
 }
 
